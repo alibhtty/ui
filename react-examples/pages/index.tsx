0b7465ba321fdca5e4944bac8e6fa490bde199b9
alibhtty <77213825+alibhtty@users.noreply.github.com>
2024-04-24 18:08:27 +0200

    1.0

---
+++ react-examples/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'next/link';
+
+const DirectoryPage = () => {
+    return <>
+        This is the directory page
+    </>
+}
+
+export default DirectoryPage;
